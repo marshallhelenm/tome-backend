b83208558e641e948f5e7071c2f624da9945253b
marshallhelenm <marshallhelenm@gmail.com>
2019-10-09 17:00:35 -0700

    log in and out is working!

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,7 +5,7 @@ class AuthController < ApplicationController
         user = User.find_by(username: login_params[:username])
         if user.authenticate(login_params[:password])
             token = encode_token({user_id: user.id})
-            render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
+            render json: { user: UserSerializer.new(user), token: token }, status: :accepted
         else
             render json: { message: 'Invalid username or password' }, status: :unauthorized
         end
