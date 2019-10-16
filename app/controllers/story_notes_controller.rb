3d14254d598c2ecce5fd8c00f2cdef8b141dc776
marshallhelenm <marshallhelenm@gmail.com>
2019-10-15 20:56:09 -0700

    story notes implemented! next step, ability to associate models in app.

--- app/controllers/story_notes_controller.rb
+++ app/controllers/story_notes_controller.rb
@@ -1,27 +1,25 @@
 class StoryNotesController < ApplicationController
     def getstory_notes
-        byebug
         story = Story.find(params['story']['id'])
         notes = story.story_notes
         render :json => notes
     end
 
     def create
-        note = StoryNote.create(title: params['note']['title'], content: ['content'], story_id: params['note']['story']['id'])
+        byebug
+        note = StoryNote.create(title: params['note']['title'], content: params['note']['content'], story_id: params['note']['story']['id'])
         render :json => note
     end
 
     def update
-        byebug
-        note = StoryNote.find(params['note']['note']['id'])
-        note.update(title: params['note']['title'], content: params['note']['content'])
+        note = StoryNote.find(params['note']['id'])
+        note.update(title: params['title'], content: params['content'])
         note.save
         render :json => note
     end
 
     def destroy
-        byebug
-        note = StoryNote.find(params['note']['note']['id'])
+        note = StoryNote.find(params['story_note']['id'])
         note.destroy
         render :json => {
             message: 'Note Deleted'
