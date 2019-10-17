0517ce941dc8fde00ee23b15f0d2bdd362f2767f
marshallhelenm <marshallhelenm@gmail.com>
2019-10-17 11:23:04 -0700

    can add characters to stories. need to block duplicate joins and add functionality to remove from story.

--- app/controllers/story_characters_controller.rb
+++ app/controllers/story_characters_controller.rb
@@ -1,5 +1,7 @@
 class StoryCharactersController < ApplicationController
     def create
         StoryCharacter.create(story_id: params['story_id'], character_id: params['character_id'])
+        story = Story.find(params['story_id'])
+        render :json => story
     end
 end
